b0e430f816a07b834c17965af2953a69560ebd6d
Rafael Freitas <gothsonn@gmail.com>
2022-11-17 21:25:40 -0300

    comit first for started by persister data in moboDB

--- src/test/java/com/accenture/pocvivoconsumersaveinvoicedata/PocVivoConsumerSaveInvoiceDataApplicationTests.java
+++ src/test/java/com/accenture/pocvivoconsumersaveinvoicedata/PocVivoConsumerSaveInvoiceDataApplicationTests.java
@@ -3,10 +3,10 @@ package com.accenture.pocvivoconsumersaveinvoicedata;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class PocVivoConsumerSaveInvoiceDataApplicationTests {
 
-    @Test
+//    @Test
     void contextLoads() {
     }
 
