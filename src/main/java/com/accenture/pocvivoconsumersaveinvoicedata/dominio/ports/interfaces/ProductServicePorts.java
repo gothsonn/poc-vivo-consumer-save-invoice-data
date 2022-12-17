5a3a0b0c6ec84ce469c1580a7e0459d00905737b
Rafael Freitas <gothsonn@gmail.com>
2022-12-16 21:43:04 -0300

    comit first for started by persister data in moboDB

--- src/main/java/com/accenture/pocvivoconsumersaveinvoicedata/dominio/ports/interfaces/ProductServicePorts.java
+++
@@ -1,9 +0,0 @@
-package com.accenture.pocvivoconsumersaveinvoicedata.dominio.ports.interfaces;
-
-import com.accenture.pocvivoconsumersaveinvoicedata.dominio.dtos.product.ProductDto;
-
-public interface ProductServicePorts {
-
-    Object findProducts();
-    void createProduct(ProductDto productDto);
-}
