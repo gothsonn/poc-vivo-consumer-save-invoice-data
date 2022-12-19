04a011d49fe0a87196fb93eb638e9b608e9a526e
c-santana-sp <eduardomarquesdesantana@gmail.com>
2022-12-18 22:25:09 -0300

    refactor: moving classes to model package

---
+++ src/main/java/com/accenture/pocvivoconsumersaveinvoicedata/dominio/dtos/ProductDto.java
@@ -0,0 +1,38 @@
+package com.accenture.pocvivoconsumersaveinvoicedata.dominio.dtos;
+
+public class ProductDto {
+
+    private String id;
+    private String name;
+    private String description;
+
+    public ProductDto(String id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
